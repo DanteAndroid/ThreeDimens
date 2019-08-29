875a9bae72bfde0ec0e386a246cc1c7092fbc21e
L <Danteandroi@gmail.com>
2019-08-29 19:03:19 +0800

    Fix bugs

--- app/src/main/java/com/example/threedimens/utils/ImageLoader.kt
+++ app/src/main/java/com/example/threedimens/utils/ImageLoader.kt
@@ -15,21 +15,36 @@ import com.example.threedimens.widget.GlideApp
  * 2019-08-27
  */
 
-fun ImageView.load(url: String, loadOnlyFromCache: Boolean = false, onLoadingFinished: () -> Unit = {}) {
+fun ImageView.load(
+    url: String,
+    loadOnlyFromCache: Boolean = false,
+    onLoadingFinished: () -> Unit = {}
+) {
     val listener = object : RequestListener<Drawable> {
-        override fun onLoadFailed(e: GlideException?, model: Any?, target: Target<Drawable>?, isFirstResource: Boolean): Boolean {
+        override fun onLoadFailed(
+            e: GlideException?,
+            model: Any?,
+            target: Target<Drawable>?,
+            isFirstResource: Boolean
+        ): Boolean {
             onLoadingFinished()
             return false
         }
 
-        override fun onResourceReady(resource: Drawable?, model: Any?, target: Target<Drawable>?, dataSource: DataSource?, isFirstResource: Boolean): Boolean {
+        override fun onResourceReady(
+            resource: Drawable?,
+            model: Any?,
+            target: Target<Drawable>?,
+            dataSource: DataSource?,
+            isFirstResource: Boolean
+        ): Boolean {
             onLoadingFinished()
             return false
         }
     }
 
-    val requestOptions = RequestOptions
-        .placeholderOf(R.drawable.loading_animation)
+    val requestOptions = RequestOptions()
+        .placeholder(R.drawable.loading_animation)
         .error(R.drawable.ic_broken_image)
         .dontTransform()
         .onlyRetrieveFromCache(loadOnlyFromCache)
